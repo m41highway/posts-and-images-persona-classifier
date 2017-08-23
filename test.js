17616c827d65161e6de1cc92e23d027309ff7926
Anthony Ng <anthony.ng.ags@gmail.com>
2017-08-23 12:00:37 +0800

    Add test user

---
+++ test.js
@@ -0,0 +1,18 @@
+// ------------------------------------------------------------------------------
+// User Id          Name                             Justification
+// ---------------  -------------------------------  ----------------------------
+// 107776466621609  Carol Albafggfhchgb Recreation   RECREATION
+// 110273219704052  Simon Albafdabchfch Innovation   INNOVATION
+// 103329973734887  Mike Albagcfecfbbh Sustainable   SUSTAINABLE
+//
+// ------------------------------------------------------------------------------
+const data = [
+    { "idx": 0, "facebook_account": "103135727087651", "justification": "RECREATION"},
+    // { "idx": 0, "facebook_account": "107776466621609", "justification": "RECREATION" },
+    // { "idx": 1, "facebook_account": "110273219704052", "justification": "INNOVATION" },
+    // { "idx": 2, "facebook_account": "103329973734887", "justification": "SUSTAINABLE" }
+];
+
+module.exports = {
+    data: data
+}
