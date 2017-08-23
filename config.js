0dc51fe878c7416287acc96d08c1fd2f91745159
Anthony Ng <anthony.ng.ags@gmail.com>
2017-08-23 11:59:40 +0800

    Add config and package.json

---
+++ config.js
@@ -0,0 +1,9 @@
+const facebook = {
+    appId: '1174269126029446',
+    appToken: '1174269126029446|5ul4NnVVbhWdj1G0SC5Cm5gO8JM',
+    threshold: 0.2
+}
+
+module.exports = {
+    facebook: facebook
+}
